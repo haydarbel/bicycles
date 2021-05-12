236179e749171e04e30f39b516f61d0557f8723d
haydar koktas <haydarkoktas@gmail.com>
2021-05-12 14:18:06 +0200

    value object

---
+++ src/main/java/be/vdab/fietsen/repositories/CampusRepository.java
@@ -0,0 +1,11 @@
+package be.vdab.fietsen.repositories;
+
+import be.vdab.fietsen.domain.Campus;
+
+import java.util.Optional;
+
+public interface CampusRepository {
+    void create(Campus campus);
+
+    Optional<Campus> findById(long id);
+}
---
+++ src/test/java/be/vdab/fietsen/repositories/JpaCampusRepositoryTest.java
@@ -0,0 +1,46 @@
+package be.vdab.fietsen.repositories;
+
+import be.vdab.fietsen.domain.Adres;
+import be.vdab.fietsen.domain.Campus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+@DataJpaTest
+@Import(JpaCampusRepository.class)
+@Sql("/insertCampus.sql")
+class JpaCampusRepositoryTest extends AbstractTransactionalJUnit4SpringContextTests {
+    private static final String CAMPUSSEN = "campussen";
+    private JpaCampusRepository repository;
+
+    public JpaCampusRepositoryTest(JpaCampusRepository repository) {
+        this.repository = repository;
+    }
+
+    private long idVanTestCampus() {
+        return jdbcTemplate.queryForObject("select id from campussen where naam = 'test'", Long.class);
+    }
+
+    @Test
+    void create() {
+        var campus = new Campus("test", new Adres("test", "test", "test", "test"));
+        repository.create(campus);
+        assertThat(countRowsInTableWhere(CAMPUSSEN, "id=" + campus.getId())).isOne();
+
+    }
+
+    @Test
+    void findById() {
+        assertThat(repository.findById(idVanTestCampus()).get().getNaam()).isEqualTo("test");
+    }
+
+    @Test
+    void findByOnbestaandeId() {
+        assertThat(repository.findById(-1)).isNotPresent();
+    }
+}
